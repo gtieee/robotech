fd480c412797bbb99214fbac50e30d5c52993097
aaronr31 <63962305+aaronr31@users.noreply.github.com>
2022-02-22 21:55:24 -0500

    experiment with hooks

--- server/routes/login.js
+++ server/routes/login.js
@@ -9,7 +9,7 @@ router.post('/', async (req, res) => {
     const pass = req.body.pass;
 
     if (!(email && pass)) {
-        res.status(400).send('Email and password data must be sent to this endpoint');
+        res.status(400).json({token: null, user: null, message: 'Email and password data must be sent to this endpoint'});
     }
 
     var userData = {};
@@ -18,20 +18,20 @@ router.post('/', async (req, res) => {
         userData = await db.query("SELECT * FROM data.users WHERE email = $1", [email]);
     } catch (err) {
         console.log(err);
-        res.status(400).send('Incorrect login information');
+        res.status(400).json({token: null, user: null});
     }
 
     if (!userData) {
-        res.status(400).send('Incorrect login information');
+        res.status(400).json({token: null, user: null, message: 'Incorrect login information'});
     }
     
     bcrypt.compare(pass, userData.rows[0].pass, (err, result) => {
         try {
-            const token = jwt.sign({user_id: email}, process.env.TOKEN_KEY, {expiresIn: '2h'});
-            res.status(200).json(token);
+            const responseToken = jwt.sign({user_id: email}, process.env.TOKEN_KEY, {expiresIn: '2h'});
+            res.status(200).json({token: responseToken, user: email, message: 'Logged in'});
         } catch (err) {
             console.log(err);
-            res.status(400).send('Failed to log in');
+            res.status(400).json({token: null, user: null, message: 'Incorrect login information'});
         }
     })
 
