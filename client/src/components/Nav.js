9bd8bff265dc318b9ad4a6e3926678b358c2eac5
aaronr31 <63962305+aaronr31@users.noreply.github.com>
2022-02-26 14:25:39 -0500

    responsive fix for nav bar

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 function Nav() {
     return (
         <ul className="nav justify-content-center p-1">
-            <li class="nav-item col-2">
+            <li class="nav-item col-sm-2">
                 <h5><Link to="/apply" class="nav-link" style={{color: "grey"}}>Apply</Link></h5>
             </li>
-            <li class="nav-item col-2">
+            <li class="nav-item col-sm-2">
                 <h5><Link to="/team" class="nav-link" style={{color: "grey"}}>Team</Link></h5>
             </li>
-            <li class="nav-item col-2">
+            <li class="nav-item col-sm-2">
                 <h5><Link to="/events" class="nav-link" style={{color: "grey"}}>Events</Link></h5>
             </li>
         </ul>
