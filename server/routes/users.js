6b80bcacb72b5005626ac94fa784658621dd1dd6
aaronr31 <63962305+aaronr31@users.noreply.github.com>
2022-03-02 21:44:28 -0500

    registration stats

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,10 +3,36 @@ const auth = require('../middleware/auth.js');
 var router = express.Router();
 
 const db = require('../db');
+const { RowDescriptionMessage } = require('pg-protocol/dist/messages');
 
 router.get('/', auth, async (req, res) => {
     const data = await db.query("SELECT * FROM users;");
     res.send(data.rows);
+})
+
+router.get('/stats', async (req, res) => {
+    if (!req.headers.authorization || (req.headers.authorization != 'rt-admin')) {
+        res.status(403).send('Unauthorized');
+        return;
+    }
+
+    var data = {registered: 0, applied: 0};
+
+    try {
+        data.registered = (await db.query("SELECT * FROM users;")).rows.length;
+    } catch {
+        res.status(500).send('Failed to fetch from users database');
+        return;
+    }
+    try {
+        data.applied = (await db.query("SELECT * FROM applications;")).rows.length;
+    } catch {
+        res.status(500).send('Failed to fetch from applications database');
+        return;
+    }
+
+    res.status(200).send(data);
+
 })
 
 router.get('/checkAuth', auth, async (req, res) => {
